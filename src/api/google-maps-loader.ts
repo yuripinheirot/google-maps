a9f5fd4a105e407597cc125962a5cb35dc64d8cc
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-03-16 13:05:10 -0300

    add initial location

--- src/api/google-maps-loader.ts
+++ src/api/google-maps-loader.ts
@@ -1,6 +1,7 @@
 import { Loader } from '@googlemaps/js-api-loader'
 
 export const googleMapsLoader = async () => {
+  const initialLocation = { lat: -5.078869240878618, lng: -42.79595746837029 }
   const loader = new Loader({
     apiKey: 'AIzaSyBefyTTZjo6Rmfqul_rGi1QLbtyQmykiNY',
     version: 'weekly',
@@ -12,8 +13,8 @@ export const googleMapsLoader = async () => {
     )) as google.maps.MapsLibrary
 
     return new Map(document.getElementById('map') as HTMLElement, {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
+      center: initialLocation,
+      zoom: 15,
     })
   })
 }
